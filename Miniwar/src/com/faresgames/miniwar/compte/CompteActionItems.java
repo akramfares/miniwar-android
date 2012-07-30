1d81e302ea6aafc121abbd897c29405cffbaf930
ayoubuto@gmail.com <ayoubuto@gmail.com@6dfc6e1b-4c1c-d301-61c3-302dcba40519>
2012-07-30 02:22:42 +0000

    Création d'une classe PlayerMW pour faciliter la gestion du player. Organisation du projet (découpage en packages). Création du package Compte pour les activités qui concernent la gestion du compte (modification, friends, consultation). Création des menus des actionbars en des fichiers XML au lieu du code Java.

--- Miniwar/src/com/faresgames/miniwar/compte/CompteActionItems.java
+++ Miniwar/src/com/faresgames/miniwar/compte/CompteActionItems.java
@@ -13,28 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.faresgames.miniwar;
+package com.faresgames.miniwar.compte;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import com.faresgames.miniwar.R;
-import com.faresgames.miniwar.compte.CompteActivity;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
-public class ActionItems extends SherlockActivity {
+import com.faresgames.miniwar.R;
+
+public class CompteActionItems extends SherlockActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Used to put dark icons on light action bar
-		 //boolean isLight = true;
+		// boolean isLight = true;
 
 		MenuInflater inflater = getSupportMenuInflater();
-		inflater.inflate(R.menu.miniwaractions, menu);
+		inflater.inflate(R.menu.compteactions, menu);
 
 		return true;
 	}
@@ -44,11 +45,15 @@ public class ActionItems extends SherlockActivity {
 		Intent i;
 		// Handle item selection
 		switch (item.getItemId()) {
-		case R.id.miniwar_account:
-			i = new Intent(ActionItems.this, CompteActivity.class);
-			startActivity(i);
-			return true;
 
+		// case R.id.compte_back:
+		// i = new Intent(CompteActionItems.this, CompteActivity.class);
+		// startActivity(i);
+		// return true;
+		
+		case R.id.compte_back:
+			this.finish();
+			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
@@ -60,8 +65,7 @@ public class ActionItems extends SherlockActivity {
 																// switching in
 																// samples
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		setContent((TextView)findViewById(R.id.MineLevel));
+		setContentView(R.layout.compte);
 	}
 
 	protected void setContent(TextView view) {
