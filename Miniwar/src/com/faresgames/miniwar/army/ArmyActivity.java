865de5b2c6e16b0228dea88a984394994625a331
belhabib.soufiane@gmail.com <belhabib.soufiane@gmail.com@6dfc6e1b-4c1c-d301-61c3-302dcba40519>
2012-08-02 01:00:47 +0000

    Army

---
+++ Miniwar/src/com/faresgames/miniwar/army/ArmyActivity.java
@@ -0,0 +1,14 @@
+package com.faresgames.miniwar.army;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArmyActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+
+	}
+	
+} 
